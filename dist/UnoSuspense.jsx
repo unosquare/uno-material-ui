97be7d930733751d936e82d5bfa0fa5f897fca29
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-03-27 14:33:06 -0600

    Fix dist with JSX

--- dist/UnoSuspense.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import { LoadingIcon } from './LoadingIcon';
-export const UnoSuspense = ({ children }) => (<React.Suspense fallback={<LoadingIcon />}>
-        {children}
-    </React.Suspense>);
