b0d3cf4d0061e8f75e7a89b88fa78acdee09ec54
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-07-16 13:40:52 -0500

    Bump

--- dist/NavBar.js
+++ dist/NavBar.js
@@ -12,12 +12,37 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var AppBar_1 = __importDefault(require("@material-ui/core/AppBar"));
 var Grid_1 = __importDefault(require("@material-ui/core/Grid"));
+var IconButton_1 = __importDefault(require("@material-ui/core/IconButton"));
 var Toolbar_1 = __importDefault(require("@material-ui/core/Toolbar"));
 var Typography_1 = __importDefault(require("@material-ui/core/Typography"));
+var Menu_1 = __importDefault(require("@material-ui/icons/Menu"));
+var makeStyles_1 = __importDefault(require("@material-ui/styles/makeStyles"));
 var React = __importStar(require("react"));
-exports.NavBar = function (props) { return (React.createElement(AppBar_1.default, { position: 'absolute', color: 'primary' },
-    React.createElement(Toolbar_1.default, null,
-        React.createElement(Grid_1.default, { container: true, direction: 'row', item: true, xs: 12 },
-            React.createElement(Grid_1.default, { item: true, xs: 6, container: true, direction: 'row', justify: 'flex-start', alignItems: 'center' },
-                React.createElement(Typography_1.default, { color: 'textSecondary', variant: 'h5' }, props.title || 'uno-react')),
-            React.createElement(Grid_1.default, { xs: 6, item: true, container: true, direction: 'row', justify: 'flex-end', alignItems: 'center' }, props.children))))); };
+var uno_react_1 = require("uno-react");
+var useStyles = makeStyles_1.default({
+    logo: {
+        marginRight: 'auto',
+        width: '200px',
+    },
+});
+var outerWidth = 600;
+var timeout = 1000;
+exports.NavBar = function (props) {
+    var classes = useStyles({});
+    var isMobileView = uno_react_1.useResolutionSwitch(outerWidth, timeout)[0];
+    var title = props.title, logo = props.logo, drawerOpen = props.drawerOpen, setDrawerOpen = props.setDrawerOpen;
+    var handleDrawerOpen = function (e) {
+        e.preventDefault();
+        setDrawerOpen(!drawerOpen);
+    };
+    return (React.createElement(AppBar_1.default, { position: 'absolute', color: 'primary' },
+        React.createElement(Toolbar_1.default, null,
+            React.createElement(Grid_1.default, { container: true, direction: 'row', item: true, xs: 12 },
+                React.createElement(Grid_1.default, { item: true, xs: 6, container: true, direction: 'row', justify: 'flex-start', alignItems: 'center' },
+                    React.createElement("img", { src: isMobileView ? '' : logo, className: classes.logo }),
+                    isMobileView &&
+                        React.createElement(IconButton_1.default, { color: 'inherit', "aria-label": 'Open drawer', onClick: handleDrawerOpen },
+                            React.createElement(Menu_1.default, null)),
+                    React.createElement(Typography_1.default, { color: 'textSecondary', variant: 'h5' }, title)),
+                React.createElement(Grid_1.default, { xs: 6, item: true, container: true, direction: 'row', justify: 'flex-end', alignItems: 'center' }, props.children)))));
+};
