1670312b605847522c4dfd3ad30196aa1a6b9db1
Alejandro Ocampo <kadosh.ivan@gmail.com>
2019-09-05 10:06:56 -0500

    Adding missing type for ValidatorForm (#85)

--- src/@types/react-form-validator-core/index.d.ts
+++ src/@types/react-form-validator-core/index.d.ts
@@ -2,4 +2,7 @@ declare module 'react-form-validator-core' {
     export class ValidatorComponent extends React.Component<any> {
         public getErrorMessage(): string;
     }
+
+    export class ValidatorForm extends React.Component<any> {
+    }
 }
