30c92b2e9517ece8a9832a3cc50aff30a2ba4fe2
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-12-17 16:00:41 -0600

    Add code coverage

--- dist/SnackbarContextProvider.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-interface ISnackbarContext {
-    sendMessage: (text: string, type?: string) => void;
-}
-export declare const SnackbarContext: React.Context<ISnackbarContext>;
-export declare const SnackbarContextProvider: React.FunctionComponent;
-export {};
