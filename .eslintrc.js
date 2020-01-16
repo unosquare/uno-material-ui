9b3836630fd1dc159f6425a6850b4d099e58cf37
Geo Perez <1775792+geoperez@users.noreply.github.com>
2020-01-16 09:21:17 -0600

    Remove documentation from this repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     }
   },
   rules: {
-    "react/prop-types": "off"
+    "react/prop-types": "off",
+    "react/display-name": "off"
   }
 };
--- srcdocs/components/LoadingIcon.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { LoadingIcon } from '../../src';
-
-export default () => (
-    <LoadingIcon color='secondary'/>
-);
